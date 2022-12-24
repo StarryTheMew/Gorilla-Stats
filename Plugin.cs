25bf8070bc6b7ea92d24339a2d88827946d29a11
StarryTheMew <121196467+StarryTheMew@users.noreply.github.com>
2022-12-24 14:00:26 -0500

    Source Code

---
+++ Plugin.cs
@@ -0,0 +1,94 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using Utilla;
+
+namespace Gorilla_Info
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        public GUIStyle guiStyle = new GUIStyle();
+        bool Done = true;
+
+        void Start()
+        {
+            /* A lot of Gorilla Tag systems will not be set up when start is called /*
+			/* Put code in OnGameInitialized to avoid null references */
+
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnEnable()
+        {
+            /* Set up your mod here */
+            /* Code here runs at the start and whenever your mod is enabled*/
+
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+            /* Undo mod setup here */
+            /* This provides support for toggling mods with ComputerInterface, please implement it :) */
+            /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+        }
+
+        void Update()
+        {
+            /* Code here runs every frame when the mod is enabled */
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is joined */
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            /* Activate your mod here */
+            /* This code will run regardless of if the mod is enabled*/
+
+            inRoom = true;
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is left */
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            /* Deactivate your mod here */
+            /* This code will run regardless of if the mod is enabled*/
+
+            inRoom = false;
+        }
+
+        private void OnGUI()
+        {
+            guiStyle.fontSize = 20;
+            guiStyle.normal.textColor = Color.white;
+
+            if (Done)
+            {
+                GUI.Label(new Rect(5f, 45f, 120f, 60f), "Max Speed: " + GorillaLocomotion.Player.Instance.maxJumpSpeed, guiStyle);
+                GUI.Label(new Rect(5f, 25f, 120f, 60f), "Speed Mult: " + GorillaLocomotion.Player.Instance.jumpMultiplier, guiStyle);
+                GUI.Label(new Rect(5f, 65f, 120f, 60f), "Status: " + GorillaTagger.Instance.currentStatus, guiStyle);
+                GUI.Label(new Rect(5f, 5f, 120f, 60f), "Created By Starry", guiStyle);
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Gorilla_Info
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.starry.gorillatag.gorilla-info";
+        public const string Name = "Gorilla_Info";
+        public const string Version = "1.0.0";
+    }
+}
